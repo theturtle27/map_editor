690537315b81956dd3e9ebb41f05e0226fd3cb4c
theturtle27 <62739849+theturtle27@users.noreply.github.com>
2022-01-25 14:52:28 +0800

    Map editor
    Init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array().fill(null),
+      obstacleIsTrue: true,
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.obstacleIsTrue ? "@" : null;
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const gridtitle = "GRID MAP";
+
+    let boardWidth = 10;
+    const row = [];
+    let k = 0; //key
+    for (let i = 0; i < boardWidth; i++) {
+      const col = [];
+      for (let j = 0; j < boardWidth; j++) {
+        col.push(this.renderSquare(boardWidth * i + j)); //push argument without {}, <>
+        k++;
+      }
+      row.push(
+        <div key={k} className="board-row">
+          {col}
+        </div>
+      );
+    }
+    return (
+      <div>
+        <div className="gridtitle">{gridtitle}</div>
+        <button
+          className="obstacle"
+          onClick={() => (this.state.obstacleIsTrue = true)}
+        >
+          Obstacle
+        </button>
+        <button
+          className="erase"
+          onClick={() => (this.state.obstacleIsTrue = false)}
+        >
+          Erase
+        </button>
+        <div>{row}</div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
